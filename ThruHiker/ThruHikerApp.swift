2ee76b575409c0816d90fc217e747f1d8de9ed20
Kai Linsley <kai.linsley@gmail.com>
2024-04-09 22:26:45 -0700

    Initial Commit

---
+++ ThruHiker/ThruHikerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThruHikerApp.swift
+//  ThruHiker
+//
+//  Created by Kai Linsley on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ThruHikerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThruHikerUITests/ThruHikerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ThruHikerUITestsLaunchTests.swift
+//  ThruHikerUITests
+//
+//  Created by Kai Linsley on 4/9/24.
+//
+
+import XCTest
+
+final class ThruHikerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
