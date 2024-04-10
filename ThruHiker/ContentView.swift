09feaac64043a2b869f255fa6555b9eafadeebbe
Kai Linsley <kai.linsley@gmail.com>
2024-04-09 22:40:15 -0700

    TEST PUSH

--- ThruHiker/ContentView.swift
+++ ThruHiker/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("thru-hiker!")
+            Text("TEST")
         }
         .padding()
     }
