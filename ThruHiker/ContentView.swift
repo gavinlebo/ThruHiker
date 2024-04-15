0ee6af6b156406effa6e8e3a42e1a2f3bb0f3f6b
Gavin Lebo <gavinlebo10@gmail.com>
2024-04-14 19:12:41 -0700

    first map up

--- ThruHiker/ContentView.swift
+++ ThruHiker/ContentView.swift
@@ -9,23 +9,100 @@ import SwiftUI
 @_spi(Experimental) import MapboxMaps
 
 
+import Foundation
+
+// Define the JSON data as an array of dictionaries
 
 
-let location = CLLocationCoordinate2D(latitude: 35.5073219, longitude: -120)
+
+
+
+
 
 
 
 struct ContentView: View {
     
+    
+    func loadJSON(from filename: String) -> Data? {
+        guard let path = Bundle.main.path(forResource: filename, ofType: "json") else {
+            print("Failed to locate \(filename).json in bundle.")
+            return nil
+        }
+
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+            return data
+        } catch {
+            print("Failed to load \(filename).json:", error)
+            return nil
+        }
+    }
+    
+    func parseJSON(data: Data) -> [String: Any]? {
+        do {
+            // make sure this JSON is in the format we expect
+            let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]{
+                if let names = json["Mile"] as? [String] {
+                    print(names)
+                }
+            }
+                // try to read out a string array
+            
+            
+        } catch let error as NSError {
+            print("Failed to load: \(error.localizedDescription)")
+            return nil
+        }
+    }
+    
+    
+    
+    func getLatLong(for mile: Double) -> (Double, Double)? {
+        let currentDirectoryURL = FileManager.default.currentDirectoryPath
+        print("Current directory: \(currentDirectoryURL)")
+        
+        if let jsonData = loadJSON(from: "Sorted_PCT_Miles"), let parsedData = parseJSON(data: jsonData) {
+            print(parsedData)
+            print("dataa\n")
+//            for data in parsedData {
+//                if let mileValue = data["Lat"] as? Double,
+//                   let latitude = data["Lat"] as? Double,
+//                   let longitude = data["Long"] as? Double,
+//                   mileValue == mile {
+//                    return (latitude, longitude)
+//                }
+//            }
+        } else {
+            print("Failed to load or parse JSON.")
+        }
+        return nil
+    }
+    
+    @State private var latitude: Double = 0.0
+    @State private var longitude: Double = 0.0
+    @State private var miles: Double = 700
+    
     var body: some View {
         Map(){
-            MapViewAnnotation(coordinate: location) {
-                Circle().fill(.blue)
-                                
-                        }
+            CircleAnnotation(centerCoordinate: CLLocationCoordinate2D(latitude: latitude, longitude: longitude))
+                .circleColor(StyleColor(.systemBlue))
+                .circleRadius(7)
+                .circleStrokeColor(StyleColor(.white))
+                .circleStrokeWidth(1)
+            
         }
-        .mapStyle(MapStyle(uri: StyleURI(rawValue: "mapbox://styles/gavinlebo/cluyo7p3n005b01og5wbi0v1t")!))
-            .ignoresSafeArea()
+        .mapStyle(MapStyle(uri: StyleURI(rawValue: "mapbox://styles/gavinlebo/cluz1mqk2005n01q17kxdbgcl")!))
+        .ignoresSafeArea()
+        .onAppear(){
+            if let latLong = getLatLong(for: miles) {
+                latitude = latLong.0
+                longitude = latLong.1
+            } else {
+                print("Latitude and Longitude not found for the given mile.")
+            }
+        }
+            
         
     }
 }
