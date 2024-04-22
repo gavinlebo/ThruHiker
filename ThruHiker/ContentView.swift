d45942307fe410211782b96c026f238d38d83438
Gavin Lebo <gavinlebo10@gmail.com>
2024-04-22 12:49:59 -0700

    integrated with taylor

--- ThruHiker/ContentView.swift
+++ ThruHiker/ContentView.swift
@@ -2,7 +2,6 @@
 //  ContentView.swift
 //  ThruHiker
 //
-//  Created by Taylor Yee on 4/14/24.
 //
 
 
@@ -17,14 +16,6 @@ import Foundation
 
 
 
-struct MileMarker: Codable {
-    let mile: Double
-    let sectionName: String
-    let long: Double
-    let lat: Double
-}
-
-
 
 
 
@@ -35,38 +26,16 @@ struct ContentView: View {
     
     @State private var latitude: Double = 0.0
     @State private var longitude: Double = 0.0
-    @State private var Mile: Double = 700
+    @State private var Mile: Double = 1000
     
     
-    func loadMiles(from jsonFileName: String) -> [MileMarker]? {
-        guard let jsonFilePath = Bundle.main.path(forResource: jsonFileName, ofType: "json") else {
-            print("JSON not found")
-            return nil
-        }
-        
-        let fileUrl = URL(fileURLWithPath: jsonFilePath)
-        do {
-            let jsonData = try Data(contentsOf: fileUrl)
-            return try JSONDecoder().decode([MileMarker].self, from: jsonData)
-        } catch {
-            print("Error loading JSON2")
-            return nil
-        }
-    }
     
-    func getCoord(coordMile mile: Double, with data: [MileMarker]) {
-        let roundedMile = (mile * 2).rounded(.down) / 2
-        if let marker = data.first(where: { $0.mile == roundedMile }) {
-            self.longitude = marker.long
-            self.latitude = marker.lat
-        }
-    }
     
     
     
     var body: some View {
         Map(){
-            CircleAnnotation(centerCoordinate: CLLocationCoordinate2D(latitude: 36.71420320000004, longitude: -118.36878639999998))
+            CircleAnnotation(centerCoordinate: CLLocationCoordinate2D(latitude: latitude, longitude: longitude))
                 .circleColor(StyleColor(.systemBlue))
                 .circleRadius(7)
                 .circleStrokeColor(StyleColor(.white))
@@ -76,11 +45,15 @@ struct ContentView: View {
         .mapStyle(MapStyle(uri: StyleURI(rawValue: "mapbox://styles/gavinlebo/cluz1mqk2005n01q17kxdbgcl")!))
         .ignoresSafeArea()
         .onAppear(){
-            if let mileMarkers = loadMiles(from: "Sorted_PCT_Miles") {
-                getCoord(coordMile: Mile, with: mileMarkers)
-            } else {
-                print("Error loading JSON")
+            print("on appear")
+            if let (long, lat) = MileMarkers.getCoord(for: Mile) {
+                print("in if")
+                self.longitude = long
+                self.latitude = lat
+                print(long)
+                print(lat)
             }
+            
             
         }
     }
