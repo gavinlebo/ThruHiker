d8e5e5525c409312cfba0ecf59285a86c7fdd136
Taylor Yee <tayloryee@Taylors-MacBook-Pro.local>
2024-04-22 11:22:52 -0700

    json parser

---
+++ File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  ThruHiker
+//
+//  Created by Taylor Yee on 4/15/24.
+//
+
+import Foundation
--- ThruHiker/ThruHiker/ContentView.swift
+++ ThruHiker/ThruHiker/ContentView.swift
@@ -1,60 +1,49 @@
-/
+
 //  ContentView.swift
 //  ThruHiker
 //
 //  Created by Taylor Yee on 4/14/24.
 //
 
+
+
 import SwiftUI
 
-struct MileMarker: Codable {
-    let mile: Double
-    let sectionName: String
-    let long: Double
-    let lat: Double
-}
-    
-    struct ContentView: View {
-        private let jsonFileName = "placeholder.json"
-        private let Mile = 1.0
-        
-        @State private var longitude: Double = 0
-        @State private var latitude: Double = 0
-        
-        var body: some View {
-            Text("placeholder for map code")
-                .onAppear {
-                    if let mileMarkers = loadMiles(from: jsonFileName) {
-                        getCoord(coordMile: Mile, with: mileMarkers)
-                    } else {
-                        print("Error loading JSON")
-                    }
-                }
-        }
+struct ContentView: View {
+    @State private var long: Double?
+    @State private var lat: Double?
+    private var mileValueToDisplay: Double = 2 //placeholder value
 
-
-        
-        func loadMiles(from jsonFileName: String) -> [MileMarker]? {
-            guard let jsonFilePath = Bundle.main.path(forResource: jsonFileName, ofType: "json") else {
-                print("JSON not found")
-                return nil
-            }
-            
-            let fileUrl = URL(fileURLWithPath: jsonFilePath)
-            do {
-                let jsonData = try Data(contentsOf: fileUrl)
-                return try JSONDecoder().decode([MileMarker].self, from: jsonData)
-            } catch {
-                print("Error loading JSON")
-                return nil
+    var body: some View {
+        VStack {
+            if let long = long, let lat = lat {
+                Text("Longitude: \(long)")
+                Text("Latitude: \(lat)")
+            } else {
+                Text("No coord found for mile value \(mileValueToDisplay)")
             }
         }
-        
-        func getCoord(coordMile mile: Double, with data: [MileMarker]) {
-            let roundedMile = (mile * 2).rounded(.down) / 2
-            if let marker = data.first(where: { $0.mile == roundedMile }) {
-                self.longitude = marker.long
-                self.latitude = marker.lat
+        .onAppear {
+            if let (long, lat) = MileMarkers.getCoord(for: mileValueToDisplay) {
+                self.long = long
+                self.lat = lat
             }
         }
     }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+
+
+
+
+
+
+
+
