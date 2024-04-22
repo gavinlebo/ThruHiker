67c4c11d3e6048661fa2a9edf89b99b594059df2
Taylor Yee <tayloryee@Taylors-MacBook-Pro.local>
2024-04-22 11:25:14 -0700

    json parser

--- ThruHiker/ThruHiker/JSONmanager.swift
+++ ThruHiker/ThruHiker/JSONmanager.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+
+
+struct MileMarkers: Codable{
+    let Section_Na: String
+    let Mile:Double
+    let Long,Lat:Double
+    
+    static let allMiles: [MileMarkers] = Bundle.main.decode(file: "example2.json") //placeholder JSON
+    
+    static func getCoord(for mile: Double) -> (Long: Double, Lat: Double)? {
+        
+            let roundedMile = (mile * 2).rounded(.down) / 2
+            if let marker = allMiles.first(where: { $0.Mile == roundedMile }) {
+                return (marker.Long, marker.Lat)
+            }
+            return nil
+        }
+    
+}
+
+
+extension Bundle {
+    func decode<T: Decodable>(file: String) -> T {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Could not find \(file) in bundle.")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Could not load \(file) from bundle.")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let loadedData = try? decoder.decode(T.self, from: data) else {
+            fatalError("Could not decode \(file) from bundle.")
+        }
+        
+        return loadedData
+    }
+}
+
+
+
