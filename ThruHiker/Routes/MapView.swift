fd72b59f879cd606690a27324a70bec80cbc7dfb
Gavin Lebo <gavinlebo10@gmail.com>
2024-05-18 12:26:00 -0700

    new routes

--- ThruHiker/Routes/MapView.swift
+++ ThruHiker/Routes/MapView.swift
@@ -37,6 +37,11 @@ struct MapView: View {
             }
             
             .mapStyle(MapStyle(uri: StyleURI(rawValue: route.mapURL)!))
+            .ornamentOptions(OrnamentOptions(
+                scaleBar: ScaleBarViewOptions(visibility: .adaptive),
+                compass: CompassViewOptions(position: .bottomLeading, visibility: .hidden)
+                ))
+
             .ignoresSafeArea()
             .onAppear {
                 //load state
@@ -145,13 +150,13 @@ struct MapView: View {
             } label: {
                 Image(systemName: "chart.bar.xaxis")
                     .resizable()
-                    .foregroundStyle(.blue, .black)
+                    .foregroundStyle(.blue)
                     .frame(width: 50, height: 50) // Adjust the frame size to make it smaller
                     .padding(10) // Add padding to create space around the image
             }
             .background(
                 RoundedRectangle(cornerRadius: 15)
-                    .fill(Color("lightBrown"))
+                    .fill(.white)
                     .frame(width: 70, height: 70) // Set the frame size for the background
             )
             .offset(x: 150, y: 320)
@@ -173,13 +178,13 @@ struct MapView: View {
                                         }) {
                                             Image(systemName: "xmark")
                                                 .foregroundColor(.white)
-                                                .padding()
+                                                //.padding()
                                                 .frame(width: 30, height: 30)
                                                 .background(Color.red)
                                                 .clipShape(Circle())
                                         }
                                     }
-                                    .padding([.top, .trailing])
+                                    //.padding([.top, .trailing])
 
                                     Text("Statistics")
                                         .font(.title)
@@ -187,12 +192,15 @@ struct MapView: View {
                                         .padding()
                                     Text("Start Date: \(self.startDate)")
                                     Text("Miles Completed: \(Int(self.Mile))")
-                                    Text("Miles Remaining: \(route.distance - Int(self.Mile))")
+                                    Text("Miles Remaining: \(Int(route.distance - self.Mile))")
+                                    Text("Expected Finish: ")
+                                    Text("Miles today: ")
+                                    Text("Most miles in day: ")
                                     
                                     Spacer()
                                 }
                                 .padding()
-                                .frame(width: 300, height: 300)
+                                .frame(width: 300, height: 350)
                                 .background(.softGreen)
                                 .cornerRadius(15)
                                 .shadow(radius: 10)
@@ -265,41 +273,97 @@ struct MapView: View {
 
 
 
+//struct SheetView: View {
+//    @ObservedObject var viewModel = FlickrViewModel()
+//    @State private var selectedPhoto: FlickrPhoto?
+//    let latitude: Double
+//    let longitude: Double
+//
+//    let columns = [
+//        GridItem(.flexible(), spacing: 20),
+//        GridItem(.flexible(), spacing: 20),
+//        GridItem(.flexible(), spacing: 20)
+//    ]
+//    
+//    var body: some View {
+//        VStack {
+//            Text("Daily Scenary")
+//                    .font(.title)
+//                    .bold()
+//                    .padding()
+//                    .foregroundStyle(.black)
+//            
+//            ScrollView {
+//                LazyVGrid(columns: columns, spacing: 20) {
+//                    ForEach(viewModel.photos) { photo in
+//                        if let url = URL(string: photo.imageUrl) {
+//                            AsyncImage(url: url) { image in
+//                                image
+//                                    .resizable()
+//                                    .aspectRatio(contentMode: .fit)
+//                                    .onTapGesture {
+//                                        selectedPhoto = photo
+//                                    }
+//                            } placeholder: {
+//                                ProgressView()
+//                            }
+//                            .frame(width: 120, height: 120)
+//                            .cornerRadius(8)
+//                            .padding(5)
+//                        }
+//                    }
+//                }
+//                .padding()
+//            }
+//        }
+//        .onAppear {
+//            viewModel.fetchPhotos(latitude: latitude, longitude: longitude)
+//        }
+//        .fullScreenCover(item: $selectedPhoto) { photo in
+//            FullScreenImageView(photo: photo)
+//        }
+//        .background(Color("softGreen"))
+//    }
+//}
+
+struct IdentifiableInt: Identifiable {
+    var id: Int
+}
+
 struct SheetView: View {
     @ObservedObject var viewModel = FlickrViewModel()
-    @State private var selectedPhoto: FlickrPhoto?
+    @State private var selectedPhotoIndex: IdentifiableInt?
     let latitude: Double
     let longitude: Double
 
     let columns = [
-        GridItem(.flexible(), spacing: 20),
-        GridItem(.flexible(), spacing: 20),
-        GridItem(.flexible(), spacing: 20)
+        GridItem(.flexible(), spacing: 10),
+        GridItem(.flexible(), spacing: 10)
     ]
     
     var body: some View {
         VStack {
             Text("Daily Scenary")
-                    .font(.title)
-                    .bold()
-                    .padding()
-                    .foregroundStyle(.black)
+                .font(.title)
+                .bold()
+                .padding()
+                .foregroundStyle(.black)
             
             ScrollView {
-                LazyVGrid(columns: columns, spacing: 20) {
-                    ForEach(viewModel.photos) { photo in
-                        if let url = URL(string: photo.imageUrl) {
+                LazyVGrid(columns: columns, spacing: 10) {
+                    ForEach(viewModel.photos.indices, id: \.self) { index in
+                        if let url = URL(string: viewModel.photos[index].imageUrl) {
                             AsyncImage(url: url) { image in
                                 image
                                     .resizable()
                                     .aspectRatio(contentMode: .fit)
                                     .onTapGesture {
-                                        selectedPhoto = photo
+                                        selectedPhotoIndex = IdentifiableInt(id: index)
                                     }
                             } placeholder: {
                                 ProgressView()
                             }
-                            .frame(width: 120, height: 120)
+                            .frame(width: 170, height: 170)
                             .cornerRadius(8)
                             .padding(5)
                         }
@@ -311,48 +375,228 @@ struct SheetView: View {
         .onAppear {
             viewModel.fetchPhotos(latitude: latitude, longitude: longitude)
         }
-        .fullScreenCover(item: $selectedPhoto) { photo in
-            FullScreenImageView(photo: photo)
+        .fullScreenCover(item: $selectedPhotoIndex) { identifiableIndex in
+            FullScreenImageView(viewModel: viewModel, selectedPhotoIndex: identifiableIndex.id)
         }
         .background(Color("softGreen"))
     }
 }
 
 
+
+
+
+//
+//struct FullScreenImageView: View {
+//    @ObservedObject var viewModel: FlickrViewModel
+//    @State private var selectedPhotoIndex: Int
+//    @Environment(\.presentationMode) var presentationMode
+//
+//    @State private var scale: CGFloat = 1.0
+//    @State private var lastScale: CGFloat = 1.0
+//
+//    init(viewModel: FlickrViewModel, selectedPhotoIndex: Int) {
+//        self.viewModel = viewModel
+//        self._selectedPhotoIndex = State(initialValue: selectedPhotoIndex)
+//    }
+//
+//    var body: some View {
+//        GeometryReader { geometry in
+//            ZStack {
+//                if let url = URL(string: "https://farm\(viewModel.photos[selectedPhotoIndex].farm).staticflickr.com/\(viewModel.photos[selectedPhotoIndex].server)/\(viewModel.photos[selectedPhotoIndex].id)_\(viewModel.photos[selectedPhotoIndex].secret)_b.jpg") {
+//                    AsyncImage(url: url) { image in
+//                        image
+//                            .resizable()
+//                            .scaledToFit()
+//                            .scaleEffect(scale)
+//                            .gesture(
+//                                MagnificationGesture()
+//                                    .onChanged { value in
+//                                        scale = lastScale * value
+//                                        if scale < 1.0{
+//                                            scale = 1.0
+//                                        }
+//                                    }
+//                                    .onEnded { _ in
+//                                        lastScale = scale
+//                                    }
+//                            )
+//                            
+//                            .edgesIgnoringSafeArea(.all)
+//                    } placeholder: {
+//                        ProgressView()
+//                            .edgesIgnoringSafeArea(.all)
+//                    }
+//                }
+//
+//                VStack {
+//                    HStack {
+//                        Spacer()
+//
+//                        Button(action: {
+//                            presentationMode.wrappedValue.dismiss()
+//                        }) {
+//                            Image(systemName: "xmark")
+//                                .foregroundColor(.white)
+//                                .padding()
+//                                .frame(width: 30, height: 30)
+//                                .background(Color.red)
+//                                .clipShape(Circle())
+//                        }
+//                    }
+//
+//                    Spacer()
+//                }
+//
+//                HStack {
+//                    Button(action: {
+//                        selectedPhotoIndex = max(selectedPhotoIndex - 1, 0)
+//                        resetScale()
+//                    }) {
+//                        Image(systemName: "chevron.left.circle.fill")
+//                            .font(.largeTitle)
+//                            .foregroundStyle(Color("lightBrown"))
+//                            //.padding()
+////                            .background(Color.black.opacity(0.6))
+////                            .clipShape(Circle())
+//                            //.padding()
+//                    }
+//                    .disabled(selectedPhotoIndex == 0)
+//
+//                    Spacer()
+//
+//                    Button(action: {
+//                        selectedPhotoIndex = min(selectedPhotoIndex + 1, viewModel.photos.count - 1)
+//                        resetScale()
+//                    }) {
+//                        Image(systemName: "chevron.right.circle.fill")
+//                            .font(.largeTitle)
+//                            .foregroundStyle(Color("lightBrown"))
+//                            //.padding()
+////                            .background(Color.black.opacity(0.6))
+////                            .clipShape(Circle())
+//                            //.padding()
+//                    }
+//                    .disabled(selectedPhotoIndex == viewModel.photos.count - 1)
+//                }
+//                .padding(.horizontal)
+//            }
+//        }
+//    }
+//
+//    private func resetScale() {
+//        scale = 1.0
+//        lastScale = 1.0
+//    }
+//}
 
 
 struct FullScreenImageView: View {
-    let photo: FlickrPhoto
+    @ObservedObject var viewModel: FlickrViewModel
+    @State private var selectedPhotoIndex: Int
     @Environment(\.presentationMode) var presentationMode
 
+    @State private var scale: CGFloat = 1.0
+    @State private var lastScale: CGFloat = 1.0
+
+    init(viewModel: FlickrViewModel, selectedPhotoIndex: Int) {
+        self.viewModel = viewModel
+        self._selectedPhotoIndex = State(initialValue: selectedPhotoIndex)
+    }
+
     var body: some View {
-        ZStack(alignment: .topTrailing) {
-            if let url = URL(string: "https://farm\(photo.farm).staticflickr.com/\(photo.server)/\(photo.id)_\(photo.secret)_b.jpg") { // _b for larger size
-                AsyncImage(url: url) { image in
-                    image
-                        .resizable()
-                        .scaledToFit()
-                        .edgesIgnoringSafeArea(.all)
-                } placeholder: {
-                    ProgressView()
-                        .edgesIgnoringSafeArea(.all)
+        GeometryReader { geometry in
+            ZStack {
+                Color.black.edgesIgnoringSafeArea(.all)
+
+                VStack {
+                    Spacer()
+
+                    if let url = URL(string: "https://farm\(viewModel.photos[selectedPhotoIndex].farm).staticflickr.com/\(viewModel.photos[selectedPhotoIndex].server)/\(viewModel.photos[selectedPhotoIndex].id)_\(viewModel.photos[selectedPhotoIndex].secret)_b.jpg") {
+                        AsyncImage(url: url) { image in
+                            image
+                                .resizable()
+                                .scaledToFit()
+                                .scaleEffect(scale)
+                                .gesture(
+                                    MagnificationGesture()
+                                        .onChanged { value in
+                                            scale = lastScale * value
+                                            if scale < 1.0{
+                                                scale = 1.0
+                                            }
+                                        }
+                                        .onEnded { _ in
+                                            lastScale = scale
+                                        }
+                                )
+                                .overlay(
+                                    HStack {
+                                        Button(action: {
+                                            selectedPhotoIndex = max(selectedPhotoIndex - 1, 0)
+                                            resetScale()
+                                        }) {
+                                            Image(systemName: "chevron.left.circle.fill")
+                                                .font(.largeTitle)
+                                                .foregroundStyle(Color("lightBrown"))
+                                        }
+                                        .disabled(selectedPhotoIndex == 0)
+
+                                        Spacer()
+
+                                        Button(action: {
+                                            selectedPhotoIndex = min(selectedPhotoIndex + 1, viewModel.photos.count - 1)
+                                            resetScale()
+                                        }) {
+                                            Image(systemName: "chevron.right.circle.fill")
+                                                .font(.largeTitle)
+                                                .foregroundStyle(Color("lightBrown"))
+                                        }
+                                        .disabled(selectedPhotoIndex == viewModel.photos.count - 1)
+                                    }
+                                    .padding(.horizontal),
+                                    alignment: .center
+                                )
+                                .overlay(
+                                    VStack {
+                                        HStack {
+                                            Spacer()
+
+                                            Button(action: {
+                                                presentationMode.wrappedValue.dismiss()
+                                            }) {
+                                                Image(systemName: "xmark")
+                                                    .foregroundColor(.white)
+                                                    .padding()
+                                                    .frame(width: 30, height: 30)
+                                                    .background(Color.red)
+                                                    .clipShape(Circle())
+                                            }
+                                        }
+                                        Spacer()
+                                    },
+                                    alignment: .topTrailing
+                                )
+                        } placeholder: {
+                            ProgressView()
+                                .edgesIgnoringSafeArea(.all)
+                        }
+                    }
+
+                    Spacer()
                 }
             }
-
-            Button(action: {
-                presentationMode.wrappedValue.dismiss()
-            }) {
-                Image(systemName: "xmark.circle.fill")
-                    .font(.largeTitle)
-                    .padding()
-                    .background(Color.black.opacity(0.6))
-                    .clipShape(Circle())
-                    .padding()
-            }
         }
-        .background(Color("softGreen"))
+    }
+
+    private func resetScale() {
+        scale = 1.0
+        lastScale = 1.0
     }
 }
+
+
+
 
 
 
