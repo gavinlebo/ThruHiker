251f0261967551b9effb5f67bf990daccfe24f02
Kai Linsley <98914767+kalinsley@users.noreply.github.com>
2024-05-24 12:43:50 -0700

    Update ThruHikerApp.swift

--- ThruHiker/ThruHikerApp.swift
+++ ThruHiker/ThruHikerApp.swift
@@ -1,17 +1,30 @@
 //
+//  testmpaboxApp.swift
+//  testmpabox
+//
+//  Created by Kai Linsley on 5/18/24.
+//
+
+//
 //  ThruHikerApp.swift
 //  ThruHiker
 //
-//  Created by Kai Linsley on 4/9/24.
+//  Created by Kai Linsley on 5/1/24.
 //
 
 import SwiftUI
 
 @main
 struct ThruHikerApp: App {
+    @StateObject private var routeManager = RouteManager(routes: routes)
+    @StateObject private var healthKitManager = HealthKitManager() // Add HealthKitManager as an observed object
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
+            .environmentObject(routeManager)
+            .environmentObject(healthKitManager)
         }
     }
 }
+
