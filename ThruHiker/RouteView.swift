022edcba693e38d9247f97496439d7800bcdf506
Kai Linsley <98914767+kalinsley@users.noreply.github.com>
2024-05-24 12:43:32 -0700

    Add files via upload

---
+++ ThruHiker/RouteView.swift
@@ -0,0 +1,24 @@
+//
+//  RouteView.swift
+//  ThruHiker
+//
+//  Created by Kai Linsley on 4/23/24.
+//
+
+import SwiftUI
+
+struct RouteView: View {
+    
+    @State var route: Route
+    
+    
+    var body: some View {
+        MapView(route: $route)
+     }
+}
+
+/*
+#Preview {
+    RouteView(route: routes[1])
+}
+*/
